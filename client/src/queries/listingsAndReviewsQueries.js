63cd15a5ccae0e50db86281d1ae62be70b7a12c9
Maximillian <maxsilvauk@gmail.com>
2022-10-31 23:46:49 +0000

    Ammend graphql query to display only the first 10

--- client/src/queries/listingsAndReviewsQueries.js
+++ client/src/queries/listingsAndReviewsQueries.js
@@ -2,7 +2,7 @@ import { gql } from '@apollo/client';
 
 const GET_LISTINGS_AND_REVIEWS = gql`
   query getListingsAndReviews {
-    listings_and_reviews {
+    listings_and_reviews(first: 10) {
       _id
       listing_url
       name
