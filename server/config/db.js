ffba4a7bf45e566dd1ad19920a976b29666ec241
Maximillian <maxsilvauk@gmail.com>
2022-10-31 23:44:41 +0000

    Update the message for db connection

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   const conn = await mongoose.connect(process.env.MONGO_URI_ATLAS);
 
-  console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+  console.log(
+    `MongoDB connected to server: ${conn.connection.host}:${conn.connection.port}`
+  );
+  console.log(`MongoDB collection name in use: ${conn.connection.name}`);
+  console.log(``), console.log(`Connected successfully`.green.bold);
 };
 
 module.exports = connectDB;
